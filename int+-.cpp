91b9a67ac8e3a4e8908d0dbd62897bd615130751
Rae-wang74 <3081608146@qq.com>
2026-09-20 20:26:33 +0800

    Delete int+-.cpp

--- int+-.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main ()
-{
-    int a,b,c,d,e;
-    cin>>a>>b>>c>>d>>e;
-    cout<<a<<"+"<<b<<"+"<<c<<"-"<<d<<"-"<<e<<"="<<a+b+c-d-e;
-    return 0;
-}
